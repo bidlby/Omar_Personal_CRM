a3f690810796a8110d35c496239aaab98cb7c4d0
bidlby <bidlby@hotmail.com>
2022-05-04 23:37:39 +0400

    up

--- OmarCrm/CRM/models.py
+++ OmarCrm/CRM/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.forms import CharField
-from numpy import save
+
 
 
 # Create your models here.
